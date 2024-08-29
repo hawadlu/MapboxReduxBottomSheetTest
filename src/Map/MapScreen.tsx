86f88dca77b737e8ccf7c4d535ad1bee811e16a8
Luke Hawinkels <hawadlu@outlook.com>
2024-08-29 12:11:47 +1200

    Base files

---
+++ src/Map/MapScreen.tsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Mapbox, {MapView} from '@rnmapbox/maps';
+
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: 'tomato',
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export function MapScreen() {
+
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
+    </View>
+  );
+};
